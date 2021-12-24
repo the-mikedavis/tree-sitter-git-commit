0a00d18455d726c8d9499176a03311c2b4e9a9b7
Michael Davis <mcarsondavis@gmail.com>
2021-12-24 15:32:47 -0600

    point directly to generated wasm on main, not docs dir

--- docs/playground.js
+++ docs/playground.js
@@ -82,7 +82,7 @@ let tree;
   async function handleLanguageChange() {
     const newLanguageName = languageSelect.value;
     if (!languagesByName[newLanguageName]) {
-      const url = `/tree-sitter-gitcommit/tree-sitter-${newLanguageName}.wasm`
+      const url = 'https://raw.githubusercontent.com/the-mikedavis/tree-sitter-git-commit/main/tree-sitter-git_commit.wasm'
       languageSelect.disabled = true;
       try {
         languagesByName[newLanguageName] = await TreeSitter.Language.load(url);
